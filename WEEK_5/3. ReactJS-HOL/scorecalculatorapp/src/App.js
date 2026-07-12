b574c1a09c36825f4d0091acea218b57caa488e4
Tangudu Jaya Krishna <jayakrishnatangudu@gmail.com>
2026-07-12 23:03:32 +0530

    Added Week 5 React exercises

---
+++ WEEK_5/3. ReactJS-HOL/scorecalculatorapp/src/App.js
@@ -0,0 +1,11 @@
+import {CalculateScore} from '../src/Components/CalculateScore';
+
+function App() {
+  return (
+    <div className="App">
+      <CalculateScore Name={"jaya krishna tangudu"} School="PVPSIT" total={34} goal={6.75} />
+    </div>
+  );
+}
+
+export default App;
